5ca64a966b1df902b36fddf5c46ccc56b49d6f00
0xDevanshx <vayukush@gmail.com>
2025-06-07 04:11:21 +0530

    Learning DOM

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,5 +1,41 @@
 const coding = ["js","rb","py","java","cpp"]
 
-coding.forEach(function (item) {
-    console.log(item);  
+// coding.forEach(function (item) {
+//     console.log(item);  
+// })
+
+// coding.forEach((item)=> {
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// })
+
+
+const myCoding = [  //object inside array
+    {
+        languageName: "javascript",
+        laguageFileName : "js"
+    },
+    {
+        languageName: "java",
+        laguageFileName : "java"
+    },
+    {
+        languageName: "python",
+        laguageFileName : "py"
+    },
+]
+
+myCoding.forEach( (item)=> {
+    console.log(item.languageName);
+    
 })
